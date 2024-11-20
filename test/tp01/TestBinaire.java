26b4436873967ff4a6abe15db96a7a606d844244
iMomobxl <mohamed.alibacha@gmail.com>
2024-11-20 10:20:51 +0100

    update test unitaire of Binaire project

--- test/tp01/TestBinaire.java
+++ test/tp01/TestBinaire.java
@@ -5,20 +5,30 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TestBinaire {
-
 	@Test
-	void testNbBitV1() {
+	void testnbBitV1() {
 		assertEquals(0, Binaire.nbBitV1(0));
 		assertEquals(1, Binaire.nbBitV1(1));
 		assertEquals(3, Binaire.nbBitV1(25));
 		assertEquals(32, Binaire.nbBitV1(-1));
 	}
-
 	@Test
-	void testNbBitV2() {
+	void testnbBitV2() {
 		assertEquals(0, Binaire.nbBitV2(0));
 		assertEquals(1, Binaire.nbBitV2(1));
 		assertEquals(3, Binaire.nbBitV2(25));
 		assertEquals(32, Binaire.nbBitV2(-1));
+	}
+	@Test
+	void testnbBitV3() {
+		assertEquals(0, Binaire.nbBitV3(0));
+		assertEquals(1, Binaire.nbBitV3(1));
+		assertEquals(3, Binaire.nbBitV3(25));
+		assertEquals(32, Binaire.nbBitV3(-1));
+	}
+	@Test
+	void estMajuscule() {
+		assertEquals(true, Binaire.estMajuscule('A'));
+		assertEquals(false, Binaire.estMajuscule('a'));
 	}
 }
