28b2e3c11c2abb4887e8a7205bf0c45f9bd8751d
iMomobxl <mohamed.alibacha@gmail.com>
2025-01-29 19:14:17 +0100

    Cours 29-01-25 triBull

--- src/tris/Tri.java
+++ src/tris/Tri.java
@@ -21,6 +21,69 @@ public class Tri {
 		
 	}
 	
+	public static void triBulleV1(int[] v) { // tri a bulles V1
+		
+	}
+	
+	public static void triBulleV2(int[] v) { // tri a bulles V2
+		int d = 1; // valeur bidon pour faire le while au minimum 1 fois
+		int fin = v.length - 2;
+		while (d > 0) {
+			d = 0;
+			for (int i = 0 ; i <= fin ; i++) {
+				if (v[i] > v[i + 1]) {
+					int tmp = v[i];
+					v[i] = v[i + 1];
+					v[i + 1] = tmp;
+					d = i;
+				}
+			}
+			fin = d - 1; 
+		}
+	}
+	
+	public static void triBulleV3(int[] v) {
+		
+		int d, g = -1;
+		int debut = 0;
+		int fin = v.length - 2;
+		
+		while (g < v.length) {
+			
+			d = 0;
+			
+			for (int i = debut ; i <= fin ; i++) {
+				
+				if (v[i] > v[i + 1]) {
+					int tmp = v[i];
+					v[i] = v[i + 1];
+					v[i + 1] = tmp;
+					d = i;
+				}
+				
+			}
+			
+			g = v.length;
+			
+			if (d > 0) { //verifie si il y a eu echange
+				
+				for (int i = d ; i > debut ; i--) {
+					
+					if (v[i - 1] > v[i]) {
+						
+						int tmp = v[i];
+						v[i] = v[i - 1];
+						v[i - 1] = tmp;
+						g = i;
+					}
+				}	
+				
+				fin = d - 1;
+				debut = g;
+			}
+			
+		}
+	}
 	
 	
 	
--- test/tris/TestTri.java
+++ test/tris/TestTri.java
@@ -26,9 +26,60 @@ public class TestTri {
 		assertArrayEquals(vectTri1, vect1);
 
 		Tri.triIns(vect2);
-		assertArrayEquals(vectTri2, vect2);
+		assertArrayEquals(vectTri2, vect2);		
+	}
+	
+	@Test
+	public void triBullesV2() {
+		
+		int[] vect00 = {4};
+		
+		int[] vect0 = {4, 2};
+		int[] vectTri0 = {2, 4};
+		
+		int[] vect1 = {4, 3, 2, 1};
+		int[] vectTri1 = {1, 2, 3, 4};
+		
+		int[] vect2 = {1};
+		int[] vectTri2 = {1};
+		
+		Tri.triBulleV2(vect00);
+		assertArrayEquals(vect00, vect00);
+		
+		Tri.triBulleV2(vect0);
+		assertArrayEquals(vectTri0, vect0);
+		
+		Tri.triBulleV2(vect1);
+		assertArrayEquals(vectTri1, vect1);
 
-				
-				
+		Tri.triBulleV2(vect2);
+		assertArrayEquals(vectTri2, vect2);	
+	}
+	
+	@Test
+	public void triBullesV3() {
+		
+		int[] vect00 = {4};
+		
+		int[] vect0 = {4, 2};
+		int[] vectTri0 = {2, 4};
+		
+		int[] vect1 = {4, 3, 2, 1};
+		int[] vectTri1 = {1, 2, 3, 4};
+		
+		int[] vect2 = {1};
+		int[] vectTri2 = {1};
+		
+		Tri.triBulleV3(vect00);
+		assertArrayEquals(vect00, vect00);
+		
+		Tri.triBulleV3(vect0);
+		assertArrayEquals(vectTri0, vect0);
+		
+		Tri.triBulleV3(vect1);
+		assertArrayEquals(vectTri1, vect1);
+
+		Tri.triBulleV3(vect2);
+		assertArrayEquals(vectTri2, vect2);	
 	}
 }
