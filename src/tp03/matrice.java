1296f2a537a3d656a4e1afb96adee48291bc31d3
iMomobxl <mohamed.alibacha@gmail.com>
2025-02-12 16:24:34 +0100

    correction exo matrice

--- src/tp03/matrice.java
+++ src/tp03/matrice.java
@@ -59,6 +59,19 @@ public class matrice {
 		return min;
 	}
 	
+	// correction rechercheMin
+	public static int rechercheMinV2(int[][] m) {
+		int mini = m[0][0];
+		for (int[] v : m) {
+			for (int elem : v) {
+				if (elem < mini) {
+					mini = elem;
+				}
+			}
+		}
+		return mini;
+	}
+	
 	public static boolean existeInMat(int[][] m, int valeur) {
 		for (int i = 0; i < m.length; i++) {
 			for (int j = 0; j < m[i].length; j++) {
@@ -68,6 +81,21 @@ public class matrice {
 			}
 		}
 		return false;
+	}
+	
+	// correction existeInMat
+	public static boolean existeInMatV2(int[][] m, int valeur) {
+		boolean trouve = false;
+		int i = 0;
+		while (!trouve && i < m.length) {
+			int j = 0;
+			while (!trouve && j < m[i].length) {
+				trouve = m[i][j] == valeur;
+				j++;
+			}
+			i++;
+		}
+		return trouve;
 	}
 		
 	public static boolean estSymmetrique(int[][] m) {
@@ -81,15 +109,32 @@ public class matrice {
 		return true;
 	}
 	
+	// correction estSymmetrique
+	public static boolean estSymmetriqueV2(int[][] m) {
+		return true;	}
+	
 	public static boolean validerSeq(int[] v, boolean[][] m) {
 		for (int i = 0; i < v.length-1; i++) {
-			System.out.println("comparaison: i=" + v[i] + " and j=" + (v[i+1]) + " is " + m[v[i]][v[i+1]]);
+//			System.out.println("comparaison: i=" + v[i] + " and j=" + (v[i+1]) + " is " + m[v[i]][v[i+1]]);
 			if (m[v[i]][v[i+1]] == false) {
 				return false;
 			}
 		}
 		return true;
 	}
+	
+	// correction validerSeq
+	public static boolean validerSeqV2(int[] v, boolean[][] m) {
+		boolean valide = true;
+		int i = 0;
+		while (valide && i < v.length - 1) {
+			valide = m[v[i]][v[i + 1]];
+			i++;
+		}
+		return valide;
+		
+	}
+
 	
 	public static int[][] produitMat(int[][]m1, int[][]m2) {
 		int[][] m = new int[m1.length][m2[0].length];
@@ -154,9 +199,12 @@ public class matrice {
 //		afficheMat(creeMat(3,3));
 //		System.out.println(maxMat(m1));
 //		System.out.println(rechercheMin(m1));
+		System.out.println(rechercheMinV2(m1));
 //		System.out.println(existeInMat(m1,3));
-		int[][] m2 = { {9, -1, 6}, {-1, 5, 1}, {5, 1, 3} };
+		System.out.println(existeInMatV2(m1, 3));
+//		int[][] m2 = { {9, -1, 6}, {-1, 5, 1}, {5, 1, 3} };
 //		System.out.println(estSymmetrique(m2));
+		System.out.println("---------------------------------");
 		boolean[][] m3 = {
 				{true, false, true, true}, 
 				{false, true, false, true},
@@ -167,7 +215,9 @@ public class matrice {
 		int[] v2 = { 0, 2, 1, 1, 3 };
 		System.out.println(validerSeq(v1, m3));
 		System.out.println(validerSeq(v2, m3));
-		
+		System.out.println(validerSeqV2(v1, m3));
+		System.out.println(validerSeqV2(v2, m3));
+		System.out.println("---------------------------------");
 		int[][] m4 = {
 				{1, 2},
 				{5, 3},
